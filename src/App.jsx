3f2b382f313559ef1be454264b131dacbe230ab8
Pavel Vasilev <pavelabadon@gmail.com>
2025-11-05 19:57:34 +0200

    BUUUUUUUUUUG

--- src/App.jsx
+++ src/App.jsx
@@ -1,126 +1,20 @@
+import NavbarStart from "./components/NavbarStart.jsx";
+import TopbarStart from "./components/TopbarStart.jsx";
+
 function App() {
 
     return (
         <main>
-            <>
+            
  
-  {/* Topbar Start */}
-  <div
-    className="container-fluid bg-primary text-white d-none d-lg-flex wow fadeIn"
-    data-wow-delay="0.1s"
-  >
-    <div className="container py-3">
-      <div className="d-flex align-items-center">
-        <a href="index.html">
-          <h2 className="text-white fw-bold m-0">WELDORK</h2>
-        </a>
-        <div className="ms-auto d-flex align-items-center">
-          <small className="ms-4">
-            <i className="fa fa-map-marker-alt me-3" />
-            123 Street, New York, USA
-          </small>
-          <small className="ms-4">
-            <i className="fa fa-envelope me-3" />
-            info@example.com
-          </small>
-          <small className="ms-4">
-            <i className="fa fa-phone-alt me-3" />
-            +012 345 67890
-          </small>
-          <div className="ms-3 d-flex">
-            <a
-              className="btn btn-sm-square btn-light text-primary ms-2"
-              href=""
-            >
-              <i className="fab fa-facebook-f" />
-            </a>
-            <a
-              className="btn btn-sm-square btn-light text-primary ms-2"
-              href=""
-            >
-              <i className="fab fa-twitter" />
-            </a>
-            <a
-              className="btn btn-sm-square btn-light text-primary ms-2"
-              href=""
-            >
-              <i className="fab fa-linkedin-in" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  {/* Topbar End */}
-  {/* Navbar Start */}
-  <div
-    className="container-fluid bg-white sticky-top wow fadeIn"
-    data-wow-delay="0.1s"
-  >
-    <div className="container">
-      <nav className="navbar navbar-expand-lg bg-white navbar-light p-lg-0">
-        <a href="index.html" className="navbar-brand d-lg-none">
-          <h1 className="fw-bold m-0">WELDORK</h1>
-        </a>
-        <button
-          type="button"
-          className="navbar-toggler me-0"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className="navbar-nav">
-            <a href="index.html" className="nav-item nav-link active">
-              Home
-            </a>
-            <a href="about.html" className="nav-item nav-link">
-              About
-            </a>
-            <a href="service.html" className="nav-item nav-link">
-              Services
-            </a>
-            <div className="nav-item dropdown">
-              <a
-                href="#"
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                Pages
-              </a>
-              <div className="dropdown-menu bg-light rounded-0 rounded-bottom m-0">
-                <a href="feature.html" className="dropdown-item">
-                  Features
-                </a>
-                <a href="team.html" className="dropdown-item">
-                  Our Team
-                </a>
-                <a href="testimonial.html" className="dropdown-item">
-                  Testimonial
-                </a>
-                <a href="appoinment.html" className="dropdown-item">
-                  Appoinment
-                </a>
-                <a href="404.html" className="dropdown-item">
-                  404 Page
-                </a>
-              </div>
-            </div>
-            <a href="contact.html" className="nav-item nav-link">
-              Contact
-            </a>
-          </div>
-          <div className="ms-auto d-none d-lg-block">
-            <a href="" className="btn btn-primary py-2 px-3">
-              Get A Quote
-            </a>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </div>
-  {/* Navbar End */}
+  <TopbarStart/>
+
+  
+  <NavbarStart/>
+  // TO DO
+      ///Като с плъгините в wordPress 
+      ////махам цятоло и оставям само двата компонента
+      /// да видим от къде гърми
   {/* Carousel Start */}
   <div className="container-fluid p-0 mb-6 wow fadeIn" data-wow-delay="0.1s">
     <div
@@ -1102,7 +996,7 @@ function App() {
   <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top">
     <i className="bi bi-arrow-up" />
   </a>
-</>
+
 
         </main>
     )

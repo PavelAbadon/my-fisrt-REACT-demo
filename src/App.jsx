6b85a0414369a1053b354954912fbc97c2c122b5
Pavel Vasilev <pavelabadon@gmail.com>
2025-11-06 13:02:14 +0200

    all primary components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import TopbarStart from "./components/TopbarStart.jsx"
 import NavbarStart from "./components/NavbarStart.jsx";
 import Carousel from "./components/Carousel.jsx";
+import About from "./components/About.jsx";
+import Features from "./components/Features.jsx";
+import OtherFeatures from "./components/OtherFeatures.jsx";
+import Service from "./components/Service.jsx";
+import Appoinment from "./components/Appoinment.jsx";
+import Team from "./components/Team.jsx";
+import Testimonial from "./components/Testimonial.jsx";
+import Newsletter from "./components/Newsletter.jsx";
+import Footer from "./components/Footer.jsx";
+import Copyright from "./components/Copyright.jsx";
+import BackToTop from "./components/BackToTop.jsx";
 
 function App() {
 
@@ -10,6 +21,17 @@ function App() {
           <TopbarStart/>
           <NavbarStart/>
           <Carousel/>
+          <About/>
+          <Features/>
+          <OtherFeatures/>
+          <Service/>
+          <Appoinment/>
+          <Team/>
+          <Testimonial/>
+          <Newsletter/>
+          <Footer/>
+          <Copyright/>
+          <BackToTop/>
 
         </main>
     )
---
+++ src/components/Copyright.jsx
@@ -0,0 +1,30 @@
+export default function Copyright(){
+    return(
+        <div className="container-fluid text-body copyright py-4">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+          ©{" "}
+          <a className="fw-semi-bold" href="#">
+            Your Site Name
+          </a>
+          , All Right Reserved.
+        </div>
+        <div className="col-md-6 text-center text-md-end">
+          {/*/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. *** /*/}
+          {/*/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, *** /*/}
+          {/*/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". *** /*/}
+          Designed By{" "}
+          <a className="fw-semi-bold" href="https://htmlcodex.com">
+            HTML Codex
+          </a>{" "}
+          Distributed by{" "}
+          <a href="https://themewagon.com" target="_blank">
+            ThemeWagon
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+    )
+}
